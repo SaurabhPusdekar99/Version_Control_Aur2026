696e4e95a736decc7d729041d67043b45e6bd3ff
Saurabh Pusdekar <saurabhpusdekar@gmail.com>
2026-09-16 23:50:50 +0530

    Modify greeting in HelloWorld.java
    Updated greeting message in HelloWorld class.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-    String A="saurabh";
+    String A="saurabh bhai kaise ho aap ";
         System.out.println("Hello World " +A);
     } 
 }
