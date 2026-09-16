1679af12742a7fc4c546615ac17f4abcc4b68743
Saurabh Pusdekar <saurabhpusdekar@gmail.com>
2026-09-16 21:28:28 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+    String A="saurabh";
+        System.out.println("Hello World " +A);
+    } 
+}
+//batch 39 in cloud devops 
